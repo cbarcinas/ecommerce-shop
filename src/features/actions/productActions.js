b82a918e0f62af59631ebc85ced3e60b1952e4d8
Craig Barcinas <cbarcinas@live.com>
2022-04-09 23:36:18 -0700

    created action constants

---
+++ src/features/actions/productActions.js
@@ -0,0 +1,6 @@
+// Action Constants
+
+export const SET_PRODUCTS = "SET_PRODUCTS";
+export const SELECTED_PRODUCT = "SELECTED_PRODUCT";
+export const REMOVE_SELECTED_PRODUCT = "REMOVE_SELECTED_PRODUCT";
+
