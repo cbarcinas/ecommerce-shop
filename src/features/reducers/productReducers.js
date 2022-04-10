29ed5c604e9699c4e9bafa94fc525e74afe26316
Craig Barcinas <cbarcinas@live.com>
2022-04-09 23:57:22 -0700

    created productsReducer

--- src/features/reducers/productReducers.js
+++ src/features/reducers/productReducers.js
@@ -2,9 +2,17 @@
 import { ActionTypes } from "../actions/productActions";
 
 // set state
-
 const initialState = {
   products: [],
 };
 
+// create reducers
 
+export const productsReducers = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case ActionTypes.SET_PRODUCTS:
+      return { ...state, products: payload };
+    default:
+      return state;
+  }
+};
