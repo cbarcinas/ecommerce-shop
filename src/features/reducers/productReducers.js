0869add8b7d0389c618bfeac4659d1582d402ec5
Craig Barcinas <cbarcinas@live.com>
2022-04-10 00:01:50 -0700

    added comments

--- src/features/reducers/productReducers.js
+++ src/features/reducers/productReducers.js
@@ -9,6 +9,8 @@ const initialState = {
 // create reducers
 
 export const productsReducers = (state = initialState, { type, payload }) => {
+    // alt: (state = initialState, action)
+    // since we know that we need type & payload from action, we can destructure them
   switch (type) {
     case ActionTypes.SET_PRODUCTS:
       return { ...state, products: payload };
