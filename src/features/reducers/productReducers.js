0fbfd2ffb6c00e1b8ac5fa1d319506268e3550c5
Craig Barcinas <cbarcinas@live.com>
2022-04-10 23:41:08 -0700

    added reducer comments

--- src/features/reducers/productReducers.js
+++ src/features/reducers/productReducers.js
@@ -1,5 +1,5 @@
 // import actions
-import { ActionTypes } from "../actions/productActions";
+import { ActionTypes } from "../actions/productsActions";
 
 // set state
 const initialState = {
@@ -9,8 +9,8 @@ const initialState = {
 // create reducers
 
 export const productsReducers = (state = initialState, { type, payload }) => {
-    // alt: (state = initialState, action)
-    // since we know that we need type & payload from action, we can destructure them
+  // alt: (state = initialState, action)
+  // since we know that we need type & payload from action, we can destructure them
   switch (type) {
     case ActionTypes.SET_PRODUCTS:
       return { ...state, products: payload };
