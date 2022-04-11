97110247910dadbde6bce40322db02af02b5fdf3
Craig Barcinas <cbarcinas@live.com>
2022-04-10 23:40:26 -0700

    created action constants

---
+++ src/features/actions/productsActions.js
@@ -0,0 +1,27 @@
+// Action Constants
+
+export const SET_PRODUCTS = "SET_PRODUCTS";
+export const SELECTED_PRODUCT = "SELECTED_PRODUCT";
+export const REMOVE_SELECTED_PRODUCT = "REMOVE_SELECTED_PRODUCT";
+
+// Action Creators
+
+export const setProducts = (products) => {
+  return {
+    type: SET_PRODUCTS,
+    payload: products,
+  };
+};
+
+export const selectedProduct = (product) => {
+  return {
+    type: SELECTED_PRODUCT,
+    payload: product,
+  };
+};
+
+export const removeSelectedProduct = () => {
+  return {
+    type: REMOVE_SELECTED_PRODUCT,
+  };
+};
