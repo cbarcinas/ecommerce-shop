08364487a20b48d9b407bcaed0b463d63187d031
Craig Barcinas <cbarcinas@live.com>
2022-04-13 10:19:08 -0700

    wip

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 // connect redux
-import store from "./features/store";
 import { Provider } from "react-redux";
+import store from "./features/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
