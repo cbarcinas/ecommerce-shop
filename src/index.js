dc065460a5cd3ebc2b11a0ee1413e28bdd6ae286
Craig Barcinas <cbarcinas@live.com>
2022-04-13 23:18:40 -0700

    connected redux store

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 // connect redux
+import store from "./app/store";
 import { Provider } from "react-redux";
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
