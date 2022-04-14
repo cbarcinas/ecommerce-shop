e999737f82cc690ca106d263c23bd90aa292bb4b
Craig Barcinas <cbarcinas@live.com>
2022-04-13 23:17:29 -0700

    created redux store

--- src/app/store.js
+++ src/app/store.js
@@ -1,1 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+
+import productsReducer from "../features/productsSlice";
+
+export default configureStore({
+  reducer: {
+    products: productsReducer,
+  },
+});
