9eceb685e165208c4225a2610a96765893c83cd2
Craig Barcinas <cbarcinas@live.com>
2022-04-15 23:00:00 -0700

    created Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import ProductListing from "./components/ProductListing";
+import ProductDetails from "./components/ProductDetails";
 
 function App() {
   return (
     <div className="app">
-      <Header />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" exact component={ProductListing} />
+          <Route path="/product/:productId" exact component={ProductDetails} />
+          <Route>404 Not Found</Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
