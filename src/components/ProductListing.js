1830ac910f123b3d68f05a85d1a5a699e1759c4c
Craig Barcinas <cbarcinas@live.com>
2022-04-19 01:29:46 -0700

    fetching data from api working: wip

--- src/components/ProductListing.js
+++ src/components/ProductListing.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 import { useSelector } from "react-redux";
 import ProductComponent from "./ProductComponent";
 
 const ProductListing = () => {
   const products = useSelector((state) => state);
 
-  console.log(products);
+  // fetch all products using async
+  const fetchProducts = async () => {
+    const res = await axios
+      .get("https://fakestoreapi.com/products")
+      .catch((err) => {
+        console.log("err", err);
+      });
+    // log our response to verify that we are getting our products
+    console.log(res);
+  };
+
+  // create our useEffect to run our fetchProdcuts func
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
     <div className="ui grid container">
       <ProductComponent />
