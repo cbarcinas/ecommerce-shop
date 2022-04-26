fee92166d2d05044d6b84d87694e9add31f46367
Craig Barcinas <cbarcinas@live.com>
2022-04-26 00:55:07 -0700

    wip

--- src/components/ProductComponent.js
+++ src/components/ProductComponent.js
@@ -4,21 +4,28 @@ import { useSelector } from "react-redux";
 const ProductComponent = () => {
   // import our products state
   const products = useSelector((state) => state.products.setProducts);
-  //
+  // create a variable containing our map function to pass into our jsx
   const renderList = products.map((product) => {
+    const { id, title, image, price, category } = product;
     return (
-      <div className="four column wide">
+      <div className="four column wide" key={id}>
         <div className="ui link cards">
           <div className="card">
-            <div className="image"></div>
+            <div className="image">
+              <img src={image} alt={title} />
+            </div>
             <div className="content">
-              <div className="header"></div>
+              <div className="header">{title}</div>
+              <div className="meta price">${price}</div>
+              <div className="meta">{category}</div>
             </div>
           </div>
         </div>
       </div>
     );
   });
+
+  return <>{renderList}</>;
 };
 
 export default ProductComponent;
