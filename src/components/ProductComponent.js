4a10481a5a7099ae9b25b12a4dee6d2393ca3473
Craig Barcinas <cbarcinas@live.com>
2022-04-26 00:49:01 -0700

    created map function to render list of products

--- src/components/ProductComponent.js
+++ src/components/ProductComponent.js
@@ -3,22 +3,22 @@ import { useSelector } from "react-redux";
 
 const ProductComponent = () => {
   // import our products state
-  const products = useSelector((state) => state.allProducts.products);
-  // We need to access id and title by destructuring it
-  // from our products array.
-  // const { title } = products[0];
-  return (
-    <div className="four column wide">
-      <div className="ui link cards">
-        <div className="card">
-          <div className="image"></div>
-          <div className="content">
-            {/* <div className="header">{title}</div> */}
+  const products = useSelector((state) => state.products.setProducts);
+  //
+  const renderList = products.map((product) => {
+    return (
+      <div className="four column wide">
+        <div className="ui link cards">
+          <div className="card">
+            <div className="image"></div>
+            <div className="content">
+              <div className="header"></div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  });
 };
 
 export default ProductComponent;
