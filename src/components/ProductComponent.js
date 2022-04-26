c2f737329a96b20650822746d1029c57134e8982
Craig Barcinas <cbarcinas@live.com>
2022-04-25 23:34:01 -0700

    wip

--- src/components/ProductComponent.js
+++ src/components/ProductComponent.js
@@ -6,14 +6,14 @@ const ProductComponent = () => {
   const products = useSelector((state) => state.allProducts.products);
   // We need to access id and title by destructuring it
   // from our products array.
-  const { title } = products[0];
+  // const { title } = products[0];
   return (
     <div className="four column wide">
       <div className="ui link cards">
         <div className="card">
           <div className="image"></div>
           <div className="content">
-            <div className="header">{title}</div>
+            {/* <div className="header">{title}</div> */}
           </div>
         </div>
       </div>
