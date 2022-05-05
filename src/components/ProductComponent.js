d0587b12be9e3cb0c4cea5c13cace7abacbeb3a8
Craig Barcinas <cbarcinas@live.com>
2022-05-05 15:27:32 -0700

    wip

--- src/components/ProductComponent.js
+++ src/components/ProductComponent.js
@@ -12,18 +12,18 @@ const ProductComponent = () => {
     return (
       <div className="four wide column" key={id}>
         <Link to={`/product/${id}`}>
-        <div className="ui link cards">
-          <div className="card">
-            <div className="image">
-              <img src={image} alt={title} />
-            </div>
-            <div className="content">
-              <div className="header">{title}</div>
-              <div className="meta price">${price}</div>
-              <div className="meta">{category}</div>
+          <div className="ui link cards">
+            <div className="card">
+              <div className="image">
+                <img src={image} alt={title} />
+              </div>
+              <div className="content">
+                <div className="header">{title}</div>
+                <div className="meta price">${price}</div>
+                <div className="meta">{category}</div>
+              </div>
             </div>
           </div>
-        </div>
         </Link>
       </div>
     );
