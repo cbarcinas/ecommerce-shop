d3b6596b5aa6b40f65716c2c8370f6975aa00379
Craig Barcinas <cbarcinas@live.com>
2022-05-08 00:05:31 -0700

    corrected key value pairs, imports/exports working

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import productsSlice from "../features/productsSlice";
+import productsReducer from "../features/productsSlice";
+import selectedProductReducer from "../features/selectedProductSlice";
 
 export default configureStore({
   reducer: {
-    allProducts: productsSlice,
+    allProducts: productsReducer,
+    product: selectedProductReducer,
   },
 });
