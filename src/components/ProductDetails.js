11826ba55036f7e90ef63346c2df167bd04cbb61
Craig Barcinas <cbarcinas@live.com>
2022-05-11 21:33:19 -0700

    displaying product mvp

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -7,10 +7,13 @@ import { selectedProduct } from "../features/selectedProductSlice";
 const ProductDetails = () => {
   const product = useSelector((state) => state.product);
   console.log(product);
+  // Destructure values that we need to display from product
+  const { image, title, price, category, description } = product;
 
   // Destructure product id from ProductComponent </Link> url
+  // console.log("product id:", productId);
   const { productId } = useParams();
-  //  console.log("product id:", productId);
+
   const dispatch = useDispatch();
 
   const fetchProductDetail = async () => {
@@ -32,8 +35,36 @@ const ProductDetails = () => {
   }, [productId]);
 
   return (
-    <div>
-      <h1>ProductDetails</h1>
+    <div className="ui grid container">
+      {Object.keys(product).length === 0 ? (
+        <div>...Loading</div>
+      ) : (
+        <div className="ui placeholder segment">
+          <div className="ui two column stackable center aligned grid">
+            <div className="ui vertical divider">AND</div>
+            <div className="middle aligned row">
+              <div className="column lp">
+                <img className="ui fluid image" src={image} />
+              </div>
+              <div className="column rp">
+                <h1>{title}</h1>
+                <h2>
+                  <a className="ui teal tag label">${price}</a>
+                </h2>
+                <h3 className="ui brown block header">{category}</h3>
+                <p>{description}</p>
+                {/* animated add to cart button */}
+                <div className="ui vertical animated button" tabIndex="0">
+                  <div className="hidden content">
+                    <i className="shop icon"></i>
+                  </div>
+                  <div className="visible content">Add to Cart</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
