0cbf5171f73beac0e307e82909850cca7d9ca3e3
Craig Barcinas <cbarcinas@live.com>
2022-05-17 23:17:32 -0700

    wip

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { MenuIcon, XIcon } from "@heroicons/react/outline";
 
 const Header = () => {
   return (
-    <div className="ui fixed menu">
-      <div className="ui container center">
-        <h2>EcommCenter</h2>
-      </div>
+    <div className="w-full">
+      <header className="max-w-[1440px] m-auto px-[5%] ">
+        <h2 className="text-3xl">EcommCenter</h2>
+        <MenuIcon className="w-5" />
+        <XIcon className="w-5" />
+      </header>
     </div>
   );
 };
