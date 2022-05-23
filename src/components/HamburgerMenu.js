7b5c2922188051f0a92c77a293ddf941accd01cb
Craig Barcinas <cbarcinas@live.com>
2022-05-23 16:38:53 -0700

    added icon imports and anchor links

--- src/components/HamburgerMenu.js
+++ src/components/HamburgerMenu.js
@@ -1,5 +1,8 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { XIcon } from "@heroicons/react/outline";
+import { ShoppingCartIcon } from "@heroicons/react/outline";
+import { UserCircleIcon } from "@heroicons/react/outline";
 
 const HamburgerMenu = (props) => {
   // Destructure props from <Header/>
@@ -8,9 +11,31 @@ const HamburgerMenu = (props) => {
   return (
     <>
       {toggleMenu ? (
-        <div>
-          <XIcon className="w-5" onClick={toggleHamburgerMenu} />
-        </div>
+        <>
+          <div>
+            <XIcon className="w-5" onClick={toggleHamburgerMenu} />
+          </div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/">Contact</Link>
+              </li>
+              <li>
+                <Link to="/">
+                  <UserCircleIcon className="w-6"></UserCircleIcon>
+                </Link>
+              </li>
+              <li>
+                <Link to="/">
+                  <ShoppingCartIcon className="w-6"></ShoppingCartIcon>
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </>
       ) : null}
     </>
   );
