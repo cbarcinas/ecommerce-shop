d38774aa6537bbae4a8db20e49d51c2ef3125630
Craig Barcinas <cbarcinas@live.com>
2022-05-23 23:56:34 -0700

    wip styling

--- src/components/HamburgerMenu.js
+++ src/components/HamburgerMenu.js
@@ -12,29 +12,39 @@ const HamburgerMenu = (props) => {
     <>
       {toggleMenu ? (
         <>
-          <div>
-            <XIcon className="w-5" onClick={toggleHamburgerMenu} />
+          <div className="absolute h-screen bg-slate-500 inset-0">
+            <div className="text-white ">
+              <XIcon className="w-7" onClick={toggleHamburgerMenu} />
+              {/* user icons */}
+              <div>
+                <ul>
+                  <li>
+                    <Link to="/">
+                      <UserCircleIcon className="w-6"></UserCircleIcon>
+                    </Link>
+                  </li>
+
+                  <li>
+                    <Link to="/">
+                      <ShoppingCartIcon className="w-6"></ShoppingCartIcon>
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+              {/* end user icons */}
+              <nav>
+                <ul>
+                  <li>
+                    <Link to="/">Home</Link>
+                  </li>
+
+                  <li>
+                    <Link to="/">Contact</Link>
+                  </li>
+                </ul>
+              </nav>
+            </div>
           </div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/">Contact</Link>
-              </li>
-              <li>
-                <Link to="/">
-                  <UserCircleIcon className="w-6"></UserCircleIcon>
-                </Link>
-              </li>
-              <li>
-                <Link to="/">
-                  <ShoppingCartIcon className="w-6"></ShoppingCartIcon>
-                </Link>
-              </li>
-            </ul>
-          </nav>
         </>
       ) : null}
     </>
