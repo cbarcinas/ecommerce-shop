88df8b86827d8e8d25671d2f6390749b6a7c52ee
Craig Barcinas <cbarcinas@live.com>
2022-05-26 00:17:30 -0700

    wip

--- src/components/ProductComponent.js
+++ src/components/ProductComponent.js
@@ -13,21 +13,26 @@ const ProductComponent = () => {
       <div className="" key={id}>
         {/* useParams will grab id in <ProductDetails/> comp */}
         <Link to={`/product/${id}`}>
-          <div className="container max-w-xs min-h-[2em] mx-auto mt-24 p-5 border-3 rounded-lg shadow-2xl ">
+          <div className="container max-w-xs mx-auto h-full mt-20 p-5 border-3 rounded-lg shadow-2xl ">
             <div>
-              <img className="mx-auto mb-10 w-64" src={image} alt={title} />
+              <img
+                className="mx-auto mb-5 w-full min-w-full h-80 object-contain"
+                src={image}
+                alt={title}
+              />
             </div>
             <div className="text-center ">
-              <p className="font-semibold tracking-wide">{title}</p>
+              <p className=" text-md leading-4 font-semibold tracking-wide">
+                {title}
+              </p>
               <p className="text-lg font-semibold italic opacity-90 tracking-wider">
                 ${price}
               </p>
-              <p>{category}</p>
               <div className="flex justify-center align-middle">
-                <button className="mt-3 mr-4 text-md font-semibold text-white tracking-wider bg-purple-400 hover:bg-purple-500 py-2 px-4 border-b-4 border-purple-300  hover:border-purple-500 rounded">
+                <button className="mt-3 mr-4 text-md font-semibold text-white  py-2 px-3 tracking-wider bg-purple-400 hover:bg-purple-500 border-b-4 border-purple-300  hover:border-purple-500 rounded cursor-pointer">
                   View
                 </button>
-                <button className="mt-3 text-md font-semibold text-white tracking-wider bg-blue-500 hover:bg-blue-600 py-2 px-4 border-b-4 border-blue-400  hover:border-blue-600  rounded">
+                <button className="mt-3 text-md font-semibold text-white py-2 px-3  tracking-wider bg-blue-500 hover:bg-blue-600 border-b-4 border-blue-400  hover:border-blue-600 rounded cursor-pointer">
                   Add to Cart
                 </button>
               </div>
