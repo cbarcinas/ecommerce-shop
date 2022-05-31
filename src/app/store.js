a37529a71b3ef894f113b2f6c0a624d56b8731ad
Craig Barcinas <cbarcinas@live.com>
2022-05-30 22:33:00 -0700

    updated store with cart slice

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productsReducer from "../features/productsSlice";
 import selectedProductReducer from "../features/selectedProductSlice";
+import cartReducer from "../features/cartSlice";
 
 export default configureStore({
   reducer: {
     allProducts: productsReducer,
     product: selectedProductReducer,
+    cart: cartReducer,
   },
 });
