3aa30ea0165fee8b5633bfd6765e2bd99f347443
Craig Barcinas <cbarcinas@live.com>
2022-05-30 22:33:07 -0700

    wip

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 function Cart() {
-  return <div>Cart</div>;
+  return (
+    <>
+      <div>
+        <h2>Shopping Cart</h2>
+        <div>
+          
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Cart;
