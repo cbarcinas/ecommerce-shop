6e221870a09b5e4940b431160506ca44bb67b971
Craig Barcinas <cbarcinas@live.com>
2022-05-30 23:32:08 -0700

    updated

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productsReducer from "../features/productsSlice";
 import selectedProductReducer from "../features/selectedProductSlice";
-import cartReducer from "../features/cartSlice";
 
 export default configureStore({
   reducer: {
     allProducts: productsReducer,
     product: selectedProductReducer,
-    cart: cartReducer,
   },
 });
