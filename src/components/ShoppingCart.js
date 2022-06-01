3f93d49394557cc913679222c73025d4b711265e
Craig Barcinas <cbarcinas@live.com>
2022-06-01 16:40:00 -0700

    wip

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -6,16 +6,36 @@ function ShoppingCart() {
   const cart = useSelector((state) => state.cart);
   console.log("shopping cart :", cart);
 
+  const { shoppingCart, totalItems, totalPrice } = cart;
+  console.log(shoppingCart);
+
   return (
+
     <>
-      <div className="absolute bg-white inset-0">
-        <div>
-          <h2 className="m-5 text-xl font-medium tracking-wide">
-            Shopping Cart
-          </h2>
-          <XIcon className="w-9 absolute top-4 right-4 cursor-pointer" />
+      {shoppingCart.length === 0 ? (
+        <div> Your cart is empty.</div>
+      ) : (
+        <div className="absolute bg-white inset-0">
+          <div>
+            <h2 className="m-5 text-xl font-medium tracking-wide">
+              Shopping Cart
+            </h2>
+            <XIcon className="w-9 absolute top-4 right-4 cursor-pointer" />
+          </div>
+          <div>
+            {shoppingCart.map((item) => {
+              const { id, title, image, price } = item;
+              return (
+                <div key={id}>
+                  <h3>{title}</h3>
+                  <img src={image} alt={title} />
+                  <p>${price}</p>
+                </div>
+              )
+            })}
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 }
