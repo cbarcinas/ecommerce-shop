6c3d1453d9b8cba95d4941ce784e167a84b785a2
Craig Barcinas <cbarcinas@live.com>
2022-05-31 22:53:16 -0700

    connected cart slice to redux store

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productsReducer from "../features/productsSlice";
 import selectedProductReducer from "../features/selectedProductSlice";
+import shoppingCartReducer from "../features/shoppingCartSlice";
 
 export default configureStore({
   reducer: {
     allProducts: productsReducer,
     product: selectedProductReducer,
+    cart: shoppingCartReducer,
   },
 });
