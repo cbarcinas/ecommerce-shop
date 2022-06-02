94a6977242fc832948d0e225fd693bd738fc6883
Craig Barcinas <cbarcinas@live.com>
2022-06-01 18:16:53 -0700

    wip: writing logic for addToCart

--- src/features/shoppingCartSlice.js
+++ src/features/shoppingCartSlice.js
@@ -9,11 +9,32 @@ export const shoppingCartSlice = createSlice({
   },
   reducers: {
     addToCart: (state, action) => {
-      state.shoppingCart.push(action.payload);
+      // We can use findIndex() method to verify if the item being
+      // added is already in the user's cart.
+      const itemIndex = state.shoppingCart.findIndex(
+        (item) => item.id === action.payload.id
+      );
+
+      // If itemIndex exists then we can add to the current count
+      // instead of adding a duplicate item to cart
+      if (itemIndex >= 0) {
+        state.shoppingCart[itemIndex].cartQuantity += 1;
+      }
+
+      // when the user adds a item to their cart, the count becomes 1
+      const tempProd = { ...action.payload, cartQuantity: 1 };
+      state.shoppingCart.push(tempProd);
+    },
+    incrementItemCount: (state) => {
+      return state.totalItems + 1;
+    },
+    decrementItemCount: (state) => {
+      return state.totalItems - 1;
     },
   },
 });
 
-export const { addToCart } = shoppingCartSlice.actions;
+export const { addToCart, incrementItemCount, decrementItemCount } =
+  shoppingCartSlice.actions;
 
 export default shoppingCartSlice.reducer;
