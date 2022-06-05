859675b97ca7a93ef0f4c07dcb4c0d6571749b7e
Craig Barcinas <cbarcinas@live.com>
2022-06-04 23:48:38 -0700

    wip

--- src/features/shoppingCartSlice.js
+++ src/features/shoppingCartSlice.js
@@ -5,7 +5,7 @@ export const shoppingCartSlice = createSlice({
   name: "cart",
   initialState: {
     shoppingCart: [],
-    cartTotalItems: 0,
+    cartQuantity: 0,
     cartTotalPrice: 0,
   },
   reducers: {
@@ -21,14 +21,17 @@ export const shoppingCartSlice = createSlice({
       // instead of adding a duplicate item to cart
       if (itemIndex >= 0) {
         state.shoppingCart[itemIndex].cartQuantity += 1;
-        toast.success("Item quantity increased");
+        toast.success(
+          `${state.shoppingCart[itemIndex].name} quantity increased`
+        );
       } else {
         // If item doesn't exist in cart, we can add it
         // When the user adds a item to their cart, the count becomes 1
-        toast.success("Item added to cart");
         const tempProd = { ...action.payload, cartQuantity: 1 };
         state.shoppingCart.push(tempProd);
+        toast.success(`${action.payload.name} added to cart`);
       }
+      console.log(itemIndex);
     },
     incrementItemCount: (state) => {
       return state.totalItems + 1;
