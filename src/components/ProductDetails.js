4c1c9c2c7007745822a2df28277ff009a231164b
Craig Barcinas <cbarcinas@live.com>
2022-06-08 22:09:49 -0700

    wip

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -7,8 +7,6 @@ import {
   removeSelectedProduct,
 } from "../features/selectedProductSlice";
 
-// *** missing funcitonality to increment/decrement amount of products being added to cart
-
 const ProductDetails = () => {
   const product = useSelector((state) => state.product);
   console.log(product);
