4aab373d715f3174f510861f881cfd39fe76dd43
Craig Barcinas <cbarcinas@live.com>
2022-06-20 02:36:48 -0700

    styles wip

--- src/components/ProductComponent.js
+++ src/components/ProductComponent.js
@@ -21,30 +21,30 @@ const ProductComponent = () => {
     return (
       <div
         key={id}
-        className="container max-w-xs h-full m-auto rounded-lg shadow-2xl"
+        className="container max-w-xs my-5 p-2 min-h-[7em] m-auto rounded-lg shadow-2xl md:mx-1 md:p-3"
       >
-        <div className="mx-5 p-5 md:mx-1 md:p-3">
+        <div>
           <div>
             <img
-              className="mx-auto mb-5 w-full min-w-full h-60 object-contain"
+              className="mx-auto mb-3 w-full min-w-full h-60 object-contain"
               src={image}
               alt={title}
             />
           </div>
-          <div className="bg-red-500 text-md text-center h-1/2 flex flex-col">
-            <div>
+          <div className=" min-h-[9em] flex flex-col justify-around">
+            <div className="flex flex-col	text-center">
               <p className="leading-4 font-semibold tracking-wide">{title}</p>
               <p className="font-semibold italic tracking-wider">${price}</p>
             </div>
-            <div className="py-2 px-3 flex justify-center">
+            <div className="flex justify-center">
               {/* useParams will grab id in <ProductDetails/> comp */}
               <Link to={`/product/${id}`}>
-                <button className="mt-3 mr-4 text-md font-semibold text-white py-2 px-3 tracking-wider bg-purple-400 hover:bg-purple-500 border-b-4 border-purple-300  hover:border-purple-500 rounded cursor-pointer">
+                <button className="mr-4 text-sm font-semibold text-white py-2 px-3 tracking-wider bg-purple-400 hover:bg-purple-500 hover:scale-x-105 border-b-4 border-purple-300  hover:border-purple-500 rounded cursor-pointer">
                   View
                 </button>
               </Link>
               <button
-                className="my-3 text-md font-semibold text-white py-2 px-3  tracking-wider bg-blue-500 hover:bg-blue-600 border-b-4 border-blue-400  hover:border-blue-600 rounded cursor-pointer"
+                className="text-sm font-semibold text-white py-2 px-3 tracking-wider bg-blue-500 hover:bg-blue-600 border-b-4 border-blue-400  hover:border-blue-600 rounded cursor-pointer"
                 onClick={() => handleAddToCart(product)}
               >
                 Add to Cart
