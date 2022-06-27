f8ea92be6468287dba9f8cf53762bde762b3b845
Craig Barcinas <cbarcinas@live.com>
2022-06-27 14:36:44 -0700

    added connection string: wip

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const server = require("./api/server");
+const mongoose = require("mongoose");
 
 const connection_string = process.env.connection_string || 5000;
 
@@ -8,3 +9,12 @@ const connection_string = process.env.connection_string || 5000;
 server.listen(port, () => {
   console.log(`\n*** Sever running on port ${port} ***\n`);
 });
+
+// To avoid dupolication errors, we can add a second param to 
+// connect method in order to handle any errors
+mongoose
+  .connect(connection_string,{
+    useNewURL
+  })
+  .then(() => console.log("MongoDB connection established..."))
+  .catch((err) => console.error("MonggoDB connection failed:", err.message));
