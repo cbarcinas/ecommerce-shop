33c5238ca63559ad893ba0e5eac5a3f5eb11f636
Craig Barcinas <cbarcinas@live.com>
2022-06-27 14:38:50 -0700

    added mongoose connect method options

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-const server = require("./api/server");
+require("dotenv").config();
 
-const port = process.env.PORT || 5000;
+const server = require("./api/server");
+const mongoose = require("mongoose");
+
+const connection_string = process.env.connection_string || 5000;
 
 // Setup server listener
 server.listen(port, () => {
   console.log(`\n*** Sever running on port ${port} ***\n`);
 });
+
+// To avoid dupolication errors, we can add a second param to 
+// connect method in order to handle any errors
+mongoose
+  .connect(connection_string,{
+    useNewURL
+  })
+  .then(() => console.log("MongoDB connection established..."))
+  .catch((err) => console.error("MonggoDB connection failed:", err.message));
