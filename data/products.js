967f6f50f2d7dbc75cdb4d33ccef0e79a46e2dc9
Craig Barcinas <cbarcinas@live.com>
2022-06-27 01:44:17 -0700

    added product wip

---
+++ data/products.js
@@ -0,0 +1,24 @@
+const products = [
+  {
+    id: 1,
+    name: "Nike Red Crewneck",
+    brand: "Nike",
+    description: "red crewneck",
+    price: 120,
+    image: [
+      "https://res.cloudinary.com/dwf0scg0g/image/upload/v1656319078/nike-red-crewneck-001.jpg_uzsilq.png",
+      "https://res.cloudinary.com/dwf0scg0g/image/upload/v1656319079/nike-red-crewneck-003.jpg_uutit5.png",
+      "https://res.cloudinary.com/dwf0scg0g/image/upload/v1656319079/nike-red-crewneck-004.jpg_kepi3c.png",
+    ],
+  },
+  {
+    id: 2,
+    name: "",
+    brand: "",
+    description: "",
+    price: 80,
+    image: [
+      
+    ],
+  },
+];
